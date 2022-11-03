d9f96d929863f1dc13052f96b7262f560b5f9087
boll84011336 <boll84011336@gmail.com>
2022-11-03 23:22:01 +0800

    fix css2

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@
 // vue.config.js 設定指南 : https://cli.vuejs.org/zh/config/#%E5%85%A8%E5%B1%80-cli-%E9%85%8D%E7%BD%AE
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? './vue-tour/'
-      : '/'
+      ? '/vue-tour/'
+      : './'
   }
